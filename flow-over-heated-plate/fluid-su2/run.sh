4f11b44d5973485a2eed47d572a40fe113d097c4
j-signorelli <jms26@illinois.edu>
2023-01-24 14:53:42 -0600

    Added run files

---
+++ flow-over-heated-plate/fluid-su2/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set -e -u
+
+python3 launch_unsteady_CHT_FlatPlate.py -f laminar_config_unsteady.cfg --parallel
---
+++ flow-over-heated-plate/solid-chyps/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -e -u
+
+chyps_initializer plate.json
+chyps_heat plate.json
