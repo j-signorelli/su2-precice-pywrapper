47fa9dd4ec2af9cfd7396f7605cf989a7b4ff457
j-signorelli <jms26@illinois.edu>
2023-01-24 15:29:13 -0600

    Reverted

--- flow-over-heated-plate/solid-chyps/run.sh
+++ flow-over-heated-plate/solid-chyps/run.sh
@@ -4,5 +4,5 @@ set -e -u
 # Set the working directory to be the one where this script is located
 cd "$(dirname "$0")"
 
-ibrun -n 25 -o 25 chyps_initializer plate.json
-ibrun -n 25 -o 25 chyps_heat plate.json
+chyps_initializer plate.json
+chyps_heat plate.json
