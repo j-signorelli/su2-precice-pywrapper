48a7c938ec7fc26c93185a4ab7f7bf45b1353d30
j-signorelli <jms26@illinois.edu>
2023-01-24 15:35:04 -0600

    Revised parallel run explanation

--- flow-over-heated-plate/solid-chyps/run.sh
+++ flow-over-heated-plate/solid-chyps/run.sh
@@ -4,5 +4,5 @@ set -e -u
 # Set the working directory to be the one where this script is located
 cd "$(dirname "$0")"
 
-chyps_initializer plate.json
+# CHyPS must first be initialized separately -- use same MPI partitions: chyps_initializer plate.json
 chyps_heat plate.json
