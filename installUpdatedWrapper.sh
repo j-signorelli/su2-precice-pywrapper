e6bdd0d6f8512f90c068c5769b3560b6b854158a
j-signorelli <jms26@illinois.edu>
2023-02-06 15:06:35 -0600

    Updated for compatibility with v7.5.1

--- installUpdatedWrapper.sh
+++ installUpdatedWrapper.sh
@@ -3,10 +3,10 @@
 
 # This auxiliary script simplifies the installation of the preCICE-adapter for SU2. It copies the adapted SU2-files to the correct locations based on the environment variable SU2_HOME.
 # Copied from SU2 Adapter
-# SU2 version: 7.5.0 "Blackbird"
+# SU2 version: 7.5.1 "Blackbird"
 
 # SU2 version for which this adapter was made
-VERSION="7.5.0 \"Blackbird\""
+VERSIONS="7.5.1 \"Blackbird\"|7.5.0 \"Blackbird\""
 
 # Always run this script from current directory
 cd "$(dirname "$0")"
@@ -18,7 +18,7 @@ printf "Necessary environment variables are set.\n"
 
 # Check for SU2 version
 printf "\nChecking whether updated Python wrapper and SU2 version are compatible..."
-if sed '1,10!d' $SU2_HOME/SU2_CFD/src/SU2_CFD.cpp | grep -Fq "$VERSION"
+if sed '1,10!d' $SU2_HOME/SU2_CFD/src/SU2_CFD.cpp | grep -Eq "$VERSION"
 then
     printf " yes.\n"
 else
--- replacement_files/python_wrapper_structure.cpp
+++ replacement_files/python_wrapper_structure.cpp
@@ -4,14 +4,14 @@
  * \file python_wrapper_structure.cpp
  * \brief Driver subroutines that are used by the Python wrapper. Those routines are usually called from an external Python environment.
  * \author D. Thomas
- * \version 7.5.0 "Blackbird"
+ * \version 7.5.1 "Blackbird"
  *
  * SU2 Project Website: https://su2code.github.io
  *
  * The SU2 Project is maintained by the SU2 Foundation
  * (http://su2foundation.org)
  *
- * Copyright 2012-2022, SU2 Contributors (cf. AUTHORS.md)
+ * Copyright 2012-2023, SU2 Contributors (cf. AUTHORS.md)
  *
  * SU2 is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -671,7 +671,7 @@ void CDriver::PrintDebugInfo() {
   }
   for (unsigned long iPoint = 0; iPoint < nPoint; iPoint++)
     for (unsigned short iVar = 0; iVar < nVar; iVar++)
-      cout << "solution_container[ZONE_0][INST_0][MESH_0][FLOW_SOL]->GetNodes()->GetSolution(" << iPoint << "," << iVar << "): " << solution_container[ZONE_0][INST_0][MESH_0][FLOW_SOL]->GetNodes()->GetSolution(iPoint, iVar) << endl;
+      cout << "solver_container[ZONE_0][INST_0][MESH_0][FLOW_SOL]->GetNodes()->GetSolution(" << iPoint << "," << iVar << "): " << solver_container[ZONE_0][INST_0][MESH_0][FLOW_SOL]->GetNodes()->GetSolution(iPoint, iVar) << endl;
       
 
 }
